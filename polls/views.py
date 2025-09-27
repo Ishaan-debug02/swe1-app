4edfc5b7d9e4ed1301b7c4d01e93c43ecd9ae6e3
Ishaan Mishra <ishaanmishra100@gmail.com>
2025-09-26 22:32:48 -0400

    Add vote

--- polls/views.py
+++ polls/views.py
@@ -14,33 +14,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    
-    # Create choices with hardcoded vote counts
-    choices_with_votes = []
-    choices = list(question.choice_set.all())
-    
-    # Predefined vote counts for each question
-    vote_data = {
-        1: [25, 18, 12, 8],   # Programming language question
-        2: [20, 15, 10, 7],   # Web framework question  
-        3: [22, 16, 14, 6]    # Operating system question
-    }
-    
-    default_votes = [15, 12, 8, 5]
-    votes = vote_data.get(question_id, default_votes)
-    
-    for i, choice in enumerate(choices):
-        choice_data = {
-            'choice_text': choice.choice_text,
-            'votes': votes[i] if i < len(votes) else 3
-        }
-        choices_with_votes.append(choice_data)
-    
-    context = {
-        'question': question,
-        'choices_with_votes': choices_with_votes
-    }
-    return render(request, 'polls/results.html', context)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
