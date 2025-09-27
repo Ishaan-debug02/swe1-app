7ad702355283a59208cb2ce4da368b5858a9d32b
Ishaan Mishra <ishaanmishra100@gmail.com>
2025-09-26 21:58:00 -0400

    Simplify views for deployment

--- polls/views.py
+++ polls/views.py
@@ -1,48 +1,34 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from polls.models import Question, Choice
-from django.utils import timezone
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
+import random
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if User.objects.count() == 0:
-            User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-            self.stdout.write('Superuser created: username=admin, password=admin123')
-        else:
-            self.stdout.write('Superuser already exists')
-            
-        # Create sample poll questions if none exist
-        if Question.objects.count() == 0:
-            # Create first question
-            q1 = Question.objects.create(
-                question_text="What's your favorite programming language?",
-                pub_date=timezone.now()
-            )
-            Choice.objects.create(question=q1, choice_text="Python", votes=0)
-            Choice.objects.create(question=q1, choice_text="JavaScript", votes=0)
-            Choice.objects.create(question=q1, choice_text="Java", votes=0)
-            Choice.objects.create(question=q1, choice_text="C++", votes=0)
-            
-            # Create second question
-            q2 = Question.objects.create(
-                question_text="Which is the best web framework?",
-                pub_date=timezone.now()
-            )
-            Choice.objects.create(question=q2, choice_text="Django", votes=0)
-            Choice.objects.create(question=q2, choice_text="React", votes=0)
-            Choice.objects.create(question=q2, choice_text="Vue.js", votes=0)
-            Choice.objects.create(question=q2, choice_text="Angular", votes=0)
-            
-            # Create third question
-            q3 = Question.objects.create(
-                question_text="What's your preferred operating system?",
-                pub_date=timezone.now()
-            )
-            Choice.objects.create(question=q3, choice_text="Windows", votes=0)
-            Choice.objects.create(question=q3, choice_text="macOS", votes=0)
-            Choice.objects.create(question=q3, choice_text="Linux", votes=0)
-            Choice.objects.create(question=q3, choice_text="Other", votes=0)
-            
-            self.stdout.write('Sample poll questions created')
-        else:
-            self.stdout.write('Poll questions already exist')
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # Simulate vote counts for demo since database is read-only
+    for choice in question.choice_set.all():
+        choice.votes = random.randint(1, 25)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # Since database is read-only, simulate the vote and redirect to results
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
