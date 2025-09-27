adca851323f767d934bfc1fa1436248536cdc0f7
Ishaan Mishra <ishaanmishra100@gmail.com>
2025-09-26 22:02:36 -0400

    Add session-based vote counting

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Choice, Question
-import random
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,9 +14,15 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # Simulate vote counts for demo since database is read-only
+    
+    # Get vote counts from session storage
+    votes = request.session.get('votes', {})
+    
+    # Apply vote counts to choices
     for choice in question.choice_set.all():
-        choice.votes = random.randint(1, 25)
+        choice_key = f"choice_{choice.id}"
+        choice.votes = votes.get(choice_key, 0)
+    
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
@@ -30,5 +35,11 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        # Since database is read-only, simulate the vote and redirect to results
+        # Store vote in session
+        votes = request.session.get('votes', {})
+        choice_key = f"choice_{selected_choice.id}"
+        votes[choice_key] = votes.get(choice_key, 0) + 1
+        request.session['votes'] = votes
+        request.session.modified = True
+        
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
