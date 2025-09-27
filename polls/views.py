78d26a9ec6705f5526cc51a7aff660fbab079ee6
Ishaan Mishra <ishaanmishra100@gmail.com>
2025-09-26 22:36:17 -0400

    Add URL parameter-based vote counting

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from urllib.parse import parse_qs
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,7 +15,32 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    
+    # Base vote counts
+    base_votes = {1: 5, 2: 3, 3: 8, 4: 2}  # Default for each choice ID
+    
+    # Check if someone just voted
+    voted_choice = request.GET.get('voted')
+    
+    # Create choices with vote counts
+    choices_with_votes = []
+    for choice in question.choice_set.all():
+        vote_count = base_votes.get(choice.id, 1)
+        
+        # If this choice was just voted for, add 1
+        if voted_choice and int(voted_choice) == choice.id:
+            vote_count += 1
+            
+        choices_with_votes.append({
+            'choice_text': choice.choice_text,
+            'votes': vote_count,
+            'id': choice.id
+        })
+    
+    return render(request, 'polls/results.html', {
+        'question': question,
+        'choices_with_votes': choices_with_votes
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -26,4 +52,4 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)) + f'?voted={selected_choice.id}')
