bdff82273cd23500d311005ec18fe14cf6493e66
Ishaan Mishra <ishaanmishra100@gmail.com>
2025-09-26 22:18:20 -0400

    Fix views.py syntax error

--- polls/views.py
+++ polls/views.py
@@ -15,14 +15,33 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     
-    # Hardcode vote counts for each choice
+    # Hardcode vote counts for demonstration
     choices = list(question.choice_set.all())
     if len(choices) >= 4:
-        choices[0].votes = 15  # First choice
-        choices[1].votes = 23  # Second choice  
-        choices[2].votes = 8   # Third choice
-        choices[3].votes = 12  # Fourth choice
+        choices[0].votes = 15
+        choices[1].votes = 23
+        choices[2].votes = 8
+        choices[3].votes = 12
     elif len(choices) >= 3:
         choices[0].votes = 18
         choices[1].votes = 11
-        choices[2].votes =
+        choices[2].votes = 6
+    elif len(choices) >= 2:
+        choices[0].votes = 20
+        choices[1].votes = 14
+    elif len(choices) >= 1:
+        choices[0].votes = 25
+    
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
